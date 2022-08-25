50436aa447b67994a7d8c2a1160e8728a5899685
Grigoryev Pavel <Green-ono@yandex.ru>
2022-08-25 15:23:01 +0300

    added jwt security User and saving it to mongo db

---
+++ src/main/java/com/grigoryev/parser/controller/AuthenticationController.java
@@ -0,0 +1,92 @@
+package com.grigoryev.parser.controller;
+
+import com.grigoryev.parser.model.User;
+import com.grigoryev.parser.repository.UserRepository;
+import com.grigoryev.parser.security.jwt.JwtTokenUtil;
+import com.grigoryev.parser.service.JwtUserDetailsService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthenticationController {
+
+    private final UserRepository userRepository;
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtUserDetailsService userDetailsService;
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestParam("user_name") String username,
+                                       @RequestParam("password") String password) {
+        Map<String, Object> responseMap = new HashMap<>();
+        try {
+            Authentication auth = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            if (auth.isAuthenticated()) {
+                log.info("{} Logged In", username);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                String token = jwtTokenUtil.generateToken(userDetails);
+                responseMap.put("message", "Logged In : " + username);
+                responseMap.put("token", token);
+                return ResponseEntity.ok(responseMap);
+            } else {
+                responseMap.put("message", "Invalid Credentials");
+                return ResponseEntity.status(401).body(responseMap);
+            }
+        } catch (DisabledException e) {
+            e.printStackTrace();
+            responseMap.put("message", "User is disabled");
+            return ResponseEntity.status(500).body(responseMap);
+        } catch (BadCredentialsException e) {
+            responseMap.put("message", "Invalid Credentials");
+            return ResponseEntity.status(401).body(responseMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+            responseMap.put("message", "Something went wrong");
+            return ResponseEntity.status(500).body(responseMap);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> saveUser(@RequestParam("first_name") String firstName,
+                                      @RequestParam("last_name") String lastName,
+                                      @RequestParam("user_name") String userName,
+                                      @RequestParam("email") String email,
+                                      @RequestParam("password") String password) {
+        Map<String, Object> responseMap = new HashMap<>();
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(new BCryptPasswordEncoder().encode(password));
+        user.setRole("USER");
+        user.setUserName(userName);
+        UserDetails userDetails = userDetailsService.createUserDetails(userName, user.getPassword());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        userRepository.save(user);
+        responseMap.put("username", userName);
+        responseMap.put("message", "Account created successfully");
+        responseMap.put("token", token);
+        return ResponseEntity.ok(responseMap);
+    }
+}
--- src/main/java/com/grigoryev/parser/security/jwt/JwtTokenUtil.java
+++ src/main/java/com/grigoryev/parser/security/jwt/JwtTokenUtil.java
@@ -3,47 +3,59 @@ package com.grigoryev.parser.security.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
+import java.io.Serializable;
+import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
-@Slf4j
-public class JwtTokenUtil {
+public class JwtTokenUtil implements Serializable {
 
-    @Value("${jwt.secret}")
-    private String jwtSecret;
+    public static final long JWT_TOKEN_VALIDITY = 18000;
 
-    /**
-     * this method is used when you need to authenticate user using a different way of doing it (e.g. controller)
-     * @param login - login of User
-     * @return generated token
-     */
-    public String generateToken(String login) {
-        Date date = Date.from(LocalDate.now().plusDays(15).atStartOfDay(ZoneId.systemDefault()).toInstant());
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String getUsernameFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
         return Jwts.builder()
-                .setSubject(login)
-                .setExpiration(date)
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(key)
                 .compact();
     }
 
-    public boolean validateToken(String token) {
-        try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            log.info("invalid token");
-        }
-        return false;
-    }
-
-    public String getLoginFromToken(String token) {
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
-        return claims.getSubject();
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 }
